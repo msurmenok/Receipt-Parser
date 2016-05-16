4f636b45f648427dda0f7397f03c2f1f0b32a9b2
Mariia <mariasurmenok@gmail.com>
2016-05-15 20:22:41 -0700

    added more exclusions

--- app/src/main/java/com/surmenok/maria/recepitparser/Exclusions.java
+++ app/src/main/java/com/surmenok/maria/recepitparser/Exclusions.java
@@ -7,11 +7,13 @@ public class Exclusions {
     public static final String[] headerExclusions = {
             "REFRIG/FROZEN", "BAKED GOODS", "PRODUCE", "MISCELLANEOUS",
             "BEAUTY/PERSONAL CARE/HYGIENE", "FOOD/BEVERAGE", "HEALTHCARE",
-            "MEAT", "DELI"
+            "MEAT", "DELI", "GROCERY", "GEN MERCHANDISE", "LIQUOR"
     };
     public static final String[] itemExclusions = {
         "TOTAL", "SUBTOTAL", "REGULAR PRICE", "CARD SAVINGS", "VISA",
-            "PAYMENT AMOUNT", "TAX", "BALANCE", "CHANGE",
+            "PAYMENT AMOUNT", "TAX", "BALANCE", "CHANGE", "REG PRICE",
+            "REGPRICE", "CARDSAV", "CARD SAV", "DEBIT CARD",
+            "MASTER  CARD"
     };
 
 }
